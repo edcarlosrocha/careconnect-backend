682291ada08a2bb6ad904161a7d716069efc1c08
Ed Carlos Rocha <edcarlos_rocha_11@hotmail.com>
2024-11-25 03:08:11 -0300

    configurado cors para atender as requisicoes do frontend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import './database/connection.js';
 import router from './routes/index.js';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
 app.use('/api', router);
 
 const PORT = process.env.PORT || 5000;
